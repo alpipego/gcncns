71c7da7fc5735f7cfb5ef4f12d09765d39fb14f3
alpipego <alpipego@gmail.com>
2017-10-18 15:49:49 +0200

    add packages to copy task

--- grunt/copy.js
+++ grunt/copy.js
@@ -3,7 +3,25 @@ module.exports = {
         files: [
             {
                 src: 'vendor/composer/ClassLoader.php',
-                dest: 'src/<% dir.thirdParty %>/Composer/Autoload/ClassLoader.php'
+                dest: 'src/<% namespaces.thirdParty %>/Composer/Autoload/ClassLoader.php'
+            },
+            {
+                expand: true,
+                cwd: 'vendor/pimple/pimple/src/Pimple',
+                src: ['**', '!**/Tests/**'],
+                dest: 'src/<% namespaces.thirdParty %>/Pimple'
+            },
+            {
+                expand: true,
+                cwd: 'vendor/psr/container/src',
+                src: '**',
+                dest: 'src/<% namespaces.thirdParty %>/Psr/Container'
+            },
+            {
+                expand: true,
+                cwd: 'vendor/wp-hibou/di/src',
+                src: '**',
+                dest: 'src/<% namespaces.thirdParty %>/WPHibou/DI'
             }
         ]
     }
