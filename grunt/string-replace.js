fc9911460131c1ea085e14127059974fc3423e27
alpipego <alpipego@gmail.com>
2017-10-18 16:15:01 +0200

    fix format for grunt variables

--- grunt/string-replace.js
+++ grunt/string-replace.js
@@ -2,20 +2,20 @@ module.exports = {
     php: {
         files: [{
             expand: true,
-            cwd: 'src/<% namespaces.thirdParty %>',
+            cwd: 'src/<%= namespaces.thirdParty %>',
             src: ['**/*.php'],
-            dest: 'src/<% namespaces.thirdParty %>',
+            dest: 'src/<%= namespaces.thirdParty %>',
             filter: 'isFile'
         }],
             options: {
             replacements: [
                 {
                     pattern: /namespace\s+?([^;]+);/,
-                    replacement: 'namespace <% namespaces.vendor %>\\<% namespaces.thirdParty %>\\$1;'
+                    replacement: 'namespace <%= namespaces.vendor %>\\<%= namespaces.thirdParty %>\\$1;'
                 },
                 {
                     pattern: /use\s+?([^;]+)(?=\\)([^;]+);/g,
-                    replacement: 'use <% namespaces.vendor %>\\<% namespaces.thirdParty %>\\$1$2;'
+                    replacement: 'use <%= namespaces.vendor %>\\<%= namespaces.thirdParty %>\\$1$2;'
                 }
             ]
         }
